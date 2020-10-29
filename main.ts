c1df72f0733f33f89065b1b43979f91c65a681d8
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2020-10-29 16:42:58 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    choicee = randint(0, 3)
+    if (choicee == 0) {
+        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    }
+    if (choicee == 1) {
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+    }
+    if (choicee == 2) {
+        music.startMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once)
+    }
+    if (choicee == 3) {
+        music.startMelody(music.builtInMelody(Melodies.Wedding), MelodyOptions.Once)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+})
+let choicee = 0
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
